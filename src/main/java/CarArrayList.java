46f7c8fce55fd56eaf4c481ed001c68f4671b8c1
Alexander <alex0nder.y@gmail.com>
2023-06-22 06:33:38 +0300

    Collections and Algorithm

--- src/main/java/CarArrayList.java
+++ src/main/java/CarArrayList.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CarArrayList implements CarList {
+    // Получение элемента по индексу - О(1)
+    // Вставка элемента в конце списка - О(1)
+    // Удаление элемента - О(N)
+    // Вставка элемента в середину или начало списка - О(N)
 
     private Car[] array = new Car[10];
     private int size = 0;
@@ -9,24 +13,33 @@ public class CarArrayList implements CarList {
     @Override
     public Car get(int index) {
         checkIndex(index);
+        // Алгоритмическая сложность операций: O(1) - операция выполняется за точное (константное)
+        // время, и не зависи от размера коллекций - лучшая алгоритмическая сложность, которая
+        // только возможна (операция происходит очень быстро)
         return array[index];
     }
 
     @Override
     public void add(Car car) {
-        if (size >= array.length) {
-            array = Arrays.copyOf(array, array.length * 2);
-
-//            Car[] newArray = new Car[array.length * 2];
-//            for (int i = 0; i < array.length; i++) {
-//                newArray[i] = array[i];
-//            }
-//            array = newArray;
-
-        }
+        increaseArray();
+        // Алгоритмическая сложность операций: O(1) - операция выполняется за точное (константное)
+        // время, и не зависи от размера коллекций - лучшая алгоритмическая сложность, которая
+        // только возможна (операция происходит очень быстро)
         array[size] = car;
         size++;
 
+    }
+
+    // Алгоритмическая сложность операций: O(N) - скорость выполнения операции напрямую зависит
+    // от размера коллекций. Чем больше элементов она содержит, тем дольше будет выполняться операция
+    @Override
+    public void add(Car car, int index) {
+        increaseArray();
+        for (int i = size; i > index; i --) {
+            array[i] = array[i - 1];
+        }
+        array[index] = car;
+        size++;
     }
 
     @Override
@@ -63,6 +76,12 @@ public class CarArrayList implements CarList {
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void increaseArray() {
+        if (size >= array.length) {
+            array = Arrays.copyOf(array, array.length * 2);
         }
     }
 }
